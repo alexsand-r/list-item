25878274f879ac8f6c2309073eb0f143134b44b2
Alex <1inboxna@gmail.com>
2024-08-07 10:33:43 +0300

    создал первый компонент боди

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+//*---
+
+import Body from "../body/body";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/body/body.js
@@ -0,0 +1,8 @@
+//*--
+import "./body.css";
+
+function Body() {
+  return <div className="body-block"></div>;
+}
+
+export default Body;
